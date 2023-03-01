b02542821c76b51c0ee1bd28d75389c8423f552a
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2023-03-01 13:16:30 -0600

    Added two new tables

--- MyShelf.DB/dbo/Tables/User.sql
+++ MyShelf.DB/dbo/Tables/User.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[User] (
-    [UserId]          INT           NOT NULL,
+    [UserId]          INT           IDENTITY (1, 1) NOT NULL,
     [Email]           VARCHAR (150) NOT NULL,
     [FirstName]       VARCHAR (75)  NOT NULL,
     [LastName]        VARCHAR (75)  NOT NULL,
@@ -7,6 +7,9 @@
     [DateJoined]      DATETIME      NOT NULL,
     [Salt]            VARCHAR (50)  NOT NULL,
     [PasswordHash]    VARCHAR (300) NOT NULL,
-    [LastLoginTime]   DATETIME      NOT NULL
+    [LastLoginTime]   DATETIME      NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC)
 );
+
+
 
---
+++ MyShelf.DB/dbo/Tables/UserSystemRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserSystemRole] (
+    [UserId]       INT NOT NULL,
+    [SystemRoleId] INT NOT NULL,
+    CONSTRAINT [PK_UserSystemRole] PRIMARY KEY CLUSTERED ([UserId] ASC, [SystemRoleId] ASC),
+    CONSTRAINT [FK_UserSystemRole_SystemRole] FOREIGN KEY ([SystemRoleId]) REFERENCES [dbo].[SystemRole] ([SystemRoleId]),
+    CONSTRAINT [FK_UserSystemRole_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId])
+);
+
