afba2234a283167b96d92e7d687510a3fbbc104f
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2023-03-01 14:50:55 -0600

    Added two new tables

---
+++ MyShelf.DB/dbo/Tables/SystemRole.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[SystemRole] (
+    [SystemRoleId]   INT          NOT NULL,
+    [SystemRoleName] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_SystemRole] PRIMARY KEY CLUSTERED ([SystemRoleId] ASC)
+);
+
---
+++ MyShelf.DB/dbo/Tables/UserSystemRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserSystemRole] (
+    [UserId]       INT NOT NULL,
+    [SystemRoldId] INT NOT NULL,
+    CONSTRAINT [PK_UserSystemRole] PRIMARY KEY CLUSTERED ([UserId] ASC, [SystemRoldId] ASC),
+    CONSTRAINT [FK_UserSystemRole_SystemRole] FOREIGN KEY ([SystemRoldId]) REFERENCES [dbo].[SystemRole] ([SystemRoleId]),
+    CONSTRAINT [FK_UserSystemRole_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId])
+);
+
