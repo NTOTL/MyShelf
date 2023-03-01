503e70be4812decb03a9caecdf646ab5aeb550d8
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2023-03-01 15:51:02 -0600

    Added two Razor pages

---
+++ MyShelf.UI/Pages/Authors/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyShelf.UI.Pages.Authors
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyShelf.UI/Pages/Authors/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyShelf.UI.Pages.Authors
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
