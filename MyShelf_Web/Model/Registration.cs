5d018d2985a690dc4945d5aa6a74c8eb88037174
officeshoebox <justafanoftoomuch@gmail.com>
2025-03-24 12:59:51 -0500

    in-class work

---
+++ MyShelf_Web/Model/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyShelf_Web.Model
+{
+    public class Registration
+    {
+        [Display(Name = "First Name")]
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }  
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
