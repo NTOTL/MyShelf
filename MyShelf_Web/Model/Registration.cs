ac1a3da1a601e85fbcf8c33a214195f66f825809
officeshoebox <justafanoftoomuch@gmail.com>
2025-03-24 13:41:58 -0500

    in-class work

--- MyShelf_Web/Model/Registration.cs
+++ MyShelf_Web/Model/Registration.cs
@@ -4,11 +4,17 @@ namespace MyShelf_Web.Model
 {
     public class Registration
     {
-        [Display(Name = "First Name")]
+        [Display(Name = "FirstName")]
+        [Required(ErrorMessage = "First Name is required.")]
         public string FirstName {  get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required.")]
         public string LastName { get; set; }
-        public string Email { get; set; }  
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required.")]
         public string ConfirmPassword { get; set; }
     }
 }
--- MyShelf_Web/Pages/Account/Register.cshtml.cs
+++ MyShelf_Web/Pages/Account/Register.cshtml.cs
@@ -6,14 +6,25 @@ namespace MyShelf_Web.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        [BindProperty]
         public Registration NewUser { get; set; } = new Registration();
         public void OnGet()
         {
-            NewUser.FirstName = "John";
+            
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
+            // Validate User Input
+            if (ModelState.IsValid)
+            {
+                // Save to database
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
